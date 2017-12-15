4a7913f8f329847a7a89f6d1af3588cc9c068010
sullay <1181518458@qq.com>
2017-12-15 16:56:42 +0800

    sullay

---
+++ web/server.js
@@ -0,0 +1,19 @@
+var http=require('http');
+var fs=require('fs');
+var url=require('url');
+//创建服务器
+http.createServer(function(request,response){
+    var pathname=url.parse(request.url).pathname;
+    console.log('Request for'+pathname+'received');
+    fs.readFile(pathname.substr(1),function(err,data){
+        if(err){
+            console.log(err);
+            response.writeHead(404,{'Context-Type':'text/html'});
+        }else{
+            response.writeHead(200,{'Context-Type':'text/html'});
+            response.write(data.toString());
+        }
+        response.end();
+    });
+}).listen(8080);
+console.log('Server running at http://127.0.0.1:8080/');
---
+++ 工具模块/domain.js
@@ -0,0 +1,26 @@
+var EventEmitter=require('events').EventEmitter;
+var domain=require('domain');
+var emitter1=new EventEmitter();
+var domain1=domain.create();
+domain1.on('error',function(err){
+    console.log('domain1处理这个错误('+err.message+')');
+});
+//显示绑定
+domain1.add(emitter1);
+emitter1.on('error',function(err){
+    console.log('监听器处理错误('+err.message+')');
+});
+emitter1.emit("error",new Error('通过监听器来处理'));
+emitter1.removeAllListeners('error');
+emitter1.emit('error',new Error('通过domain1处理'));
+var domain2=domain.create();
+domain2.on('error',function(err){
+    console.log('domain2处理这个错误('+err.message+')');
+});
+//隐式绑定
+domain2.run(function(){
+    var emitter2=new EventEmitter();
+    emitter2.emit('error',new Error('通过domain2处理'));
+});
+domain1.remove(emitter1);
+emitter1.emit('error',new Error('转化为异常，系统将崩溃'));
